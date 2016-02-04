b72166de7f4e994031b5bcf28bda468ac7d46faf
Pranay Ankit <pranayankit414@gmail.com>
2016-02-04 23:41:37 +0530

    Create Ceaser_code.c

---
+++ Ceaser_code.c
@@ -0,0 +1,43 @@
+/* Ceaser cipher created by Pranay Ankit Tiru */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+  int n, i;
+
+  printf("Enter the value of n\n");
+  scanf("%d",&n);
+
+  char a[n];
+  printf("Enter the string\n");
+  scanf("%s",a);
+
+  int ch;
+  printf("Enter 1. For encryption 2. For decryption ");
+  scanf("%d",&ch);
+  switch(ch)
+  {
+    case 1:  for(i=0;i<n;i++)
+               {
+                 if(a[i]>=97&&a[i]<122)
+                  {
+                    a[i]=a[i]+2;
+                 }
+               }
+                 printf("Coded string is ");
+                 printf("%s",a);
+             break;
+    case 2:  for(i=0;i<n;i++)
+               {
+                 if(a[i]>=97&&a[i]<122)
+                  {
+                    a[i]=a[i]-2;
+                 }
+               }
+                 printf("Decoded string is ");
+                 printf("%s",a);
+             break;
+    default:  exit(0);
+  }
+  return(0);
+}
